d6e496ddf59a65fc64b6e4ed45bb206406111311
rashmirekha99 <kothalawalarr.19@uom.lk>
2025-02-06 19:03:20 +0530

    init

---
+++ android/app/src/main/kotlin/com/rashmi/my_demo_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.rashmi.my_demo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/view/pages/splash_screen.dart
@@ -0,0 +1,25 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:my_demo_app/main.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      disableNavigation: true,
+      splash: Center(
+        child: Lottie.asset('assets/animations/new_logo.json'),
+      ),
+      nextScreen: MyHomePage(title: 'dsb'),
+      splashIconSize: 450,
+    );
+  }
+}
